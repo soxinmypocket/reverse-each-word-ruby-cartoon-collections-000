a08e9720511e3f27dcbb51640ab03364ebec83de
mathematical-integer-7084 <shawnaejohnson6@gmail.com>
2020-03-16 07:51:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,9 @@
-reverse_each_word("Hello there, how are you?")
+
+reverse_each_word = [ "Hello", "there",",","how", "are", "you", "?"]
+  reverse_each_word.collect do |word|
+    word.reverse
+  end
+end
+
+
+  ]("Hello there, how are you?")
